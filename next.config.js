ad2e6ecc3200d20a7a5e8cfb2a4d8309d2dcaae2
Hoan Le <hoanle@xtaypro.com>
2020-06-18 03:20:31 +0700

    Edit config file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
     env: {
-        OPEN_WEATHER_API_KEY: "96ce736f04bfdeb7cee236af55800f0d",
-        GEO_CODE_API_KEY: "pk.eyJ1IjoibGVob2FuMDIxMiIsImEiOiJja2JoZHQzbzQwNGRnMnNsdHlraTV4bmhxIn0.7pXnql4x9UUzKjLELwSlRA"
+        OPEN_WEATHER_API_KEY: process.env.OPEN_WEATHER_API_KEY,
+        GEO_CODE_API_KEY: process.env.GEO_CODE_API_KEY
     }
 }
