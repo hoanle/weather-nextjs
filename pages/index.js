e7d2180eb327f1712cc1a5a31a3ed8e1d9f92ace
Hoan Le <hoanle@xtaypro.com>
2020-06-18 04:35:24 +0700

    SSR

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,11 @@ import { useRouter } from 'next/router'
 import getGeocode from './../ulti/geocode';
 import getForecast from './../ulti/forecast';
 
-const HomePage = () => {
+const HomePage = (props) => {
   const router = useRouter()
   const [city, setCity] = useState("");
   const [forecast, setForecast] = useState(null)
   console.log(`city ${router.query.city}`)
-
-  if (city === "" && router.query.city !== undefined) {
-    console.log("city != " + router.query.city);
-    setCity(router.query.city)
-  }
 
   const callApi = async () => {
     const location = await getGeocode(city)
@@ -25,12 +20,24 @@ const HomePage = () => {
     console.log("useEffect " + city);
     if (city !== "") {
       console.log("should handle api here " + city);
-      callApi();
     }
-  }, [city])
+  }, [props.city])
 
   
-  return <HomePageComponent city={city} forecast={forecast} greeting="Hello! Enter the name of your place to get weather forecast"/>
+  return <HomePageComponent city={props.city} forecast={props.forecast} greeting="Hello! Enter the name of your place to get weather forecast"/>
 }
+
+export async function getServerSideProps(context) {
+  if (context != null && context.query != null && context.query.city != undefined) {
+    let city = context.query.city
+    const location = await getGeocode(city)
+    const forecast = await getForecast(location.geometry.coordinates[1], location.geometry.coordinates[0])
+    return { props: { city: context.query.city, forecast: forecast } }
+  } else {
+    return { props: { city: "", forecast: null } }
+  }
+  
+}
+
 
 export default HomePage
